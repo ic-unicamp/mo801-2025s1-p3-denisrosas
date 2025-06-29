61b465d5750275d8cc902d6fc02fe968dd7014b1
Denis Rosas <denis.rosas@gmail.com>
2025-06-28 21:21:12 -0300

    Comitando a mudança de loop unrolling

--- x86_64_code_opt_testing/fully_connected_layer.c
+++ x86_64_code_opt_testing/fully_connected_layer.c
@@ -7,15 +7,16 @@ int fully_connected_layer() {
     // 3. Executando os 3 Passos do Forward Pass:
 
     // Passo 1: Multiplicacao Matriz-Vetor (z = W * x)
+    // printf("Passo 1: Multiplicacao Matriz-Vetor (W * x)...\n");
     for (int i = 0; i < N; i++) { // Para cada neuronio de saida
-        float sum = 0.0f;
-        
-        // Multiplica linha i de W por vetor x
-        for (int j = 0; j < N; j++) { 
-            sum += W[i][j] * x[j];
+        float sum[4] = {0.0f, 0.0f, 0.0f, 0.0f}; // Inicializa a soma para cada grupo de 4 elementos
+        for (int j = 0; j < N; j=j+4) { // Multiplica linha i de W por vetor x
+            sum[0] += W[i][j] * x[j];
+            sum[1] += W[i][j+1] * x[j+1];
+            sum[2] += W[i][j+2] * x[j+2];
+            sum[3] += W[i][j+3] * x[j+3];
         }
-        // Armazena o resultado em z[i]
-        z[i] = sum;
+        z[i] = sum[0] + sum[1] + sum[2] + sum[3]; // Soma os produtos
     }
 
     // Passo 2: Adicao do Bias (z = z + b)
